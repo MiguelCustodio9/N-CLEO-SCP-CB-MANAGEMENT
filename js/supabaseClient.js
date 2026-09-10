2089b9c265d39e3ce4c2cdf912f1878a7d308217
Miguel Custódio <153029138+MiguelCustodio9@users.noreply.github.com>
2026-09-10 18:54:57 +0000

    ds

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -9,4 +9,5 @@
 const SUPABASE_URL = "https://npajzelrqggeczzswflj.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wYWp6ZWxycWdnZWN6enN3ZmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNTgzMzIsImV4cCI6MjEwNDYzNDMzMn0.QFS838miIo0a2cQpCGXfnM-8d-E1lfXWJ-_k3G-NZFQ";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClienteCriado = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabase = supabaseClienteCriado;
